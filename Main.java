67dd3065b86657b91e5f4dc72f756e4215a6bdf6
schielrh <89650296+schielrh@users.noreply.github.com>
2021-09-16 19:54:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    Random r = new Random();
+    boolean isGuessed = false;
+    int userBound;
+    int toGuess;
+    int numGuesses = 0;
+
+    System.out.println("Welcome to the guessing game!");
+    System.out.println("Please enter any positive whole number:");
+    userBound = s.nextInt();
+    
+  }
+}
